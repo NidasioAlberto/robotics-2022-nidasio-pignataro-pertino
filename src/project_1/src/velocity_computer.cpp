d40621ac953caf5d53eb84b6145be71585bc6a62
Alberto Nidasio <alberto.nidasio@mail.polimi.it>
2022-05-01 10:34:00 +0200

    [DynamicReconfigure] Corrected robot parameters dynamic reconfiguration
    Co-authored-by: Matteo Pignataro <trainer400@users.noreply.github.com>
    Co-authored-by: Paolo <paolopertino@users.noreply.github.com>

--- src/project_1/src/velocity_computer.cpp
+++ src/project_1/src/velocity_computer.cpp
@@ -1,11 +1,12 @@
 #include <dynamic_reconfigure/server.h>
 #include <geometry_msgs/TwistStamped.h>
-#include <project_1/wheelsDataSourceConfig.h>
+#include <project_1/velocityComputerParamsConfig.h>
 #include <ros/console.h>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 
 #include <Eigen/Core>
+#include <string>
 
 #include "shared/VelocityComputer.h"
 
@@ -13,23 +14,26 @@ using namespace ros;
 using namespace sensor_msgs;
 using namespace geometry_msgs;
 using namespace Eigen;
+using namespace dynamic_reconfigure;
+using namespace project_1;
 
 Publisher pub;
 
 void wheelStateCallback(const JointState::ConstPtr &msg);
-void wheelsDataSourceChangeCallback(project_1::wheelsDataSourceConfig &config,
-                                    uint32_t level);
+void velocityParametersChangeCallback(velocityComputerParamsConfig &config,
+                                      uint32_t level);
 
 int main(int argc, char **argv)
 {
     init(argc, argv, "velocity_computer");
     NodeHandle handle;
 
-    dynamic_reconfigure::Server<project_1::wheelsDataSourceConfig> dynServer;
-    dynamic_reconfigure::Server<project_1::wheelsDataSourceConfig>::CallbackType
-        callbackFunction;
-    callbackFunction = boost::bind(&wheelsDataSourceChangeCallback, _1, _2);
-    dynServer.setCallback(callbackFunction);
+    // Robot paramters recofiguration
+    Server<velocityComputerParamsConfig> velocityParamsServer;
+    Server<velocityComputerParamsConfig>::CallbackType velocityParamsCallback;
+    velocityParamsCallback =
+        boost::bind(&velocityParametersChangeCallback, _1, _2);
+    velocityParamsServer.setCallback(velocityParamsCallback);
 
     Subscriber sub = handle.subscribe("wheel_states", 1000, wheelStateCallback);
     pub            = handle.advertise<TwistStamped>("cmd_vel", 1000);
@@ -51,19 +55,47 @@ void wheelStateCallback(const JointState::ConstPtr &msg)
     pub.publish(twistMsg);
 }
 
-void wheelsDataSourceChangeCallback(project_1::wheelsDataSourceConfig &config,
-                                    uint32_t level)
+void velocityParametersChangeCallback(velocityComputerParamsConfig &config,
+                                      uint32_t level)
 {
-    ROS_INFO("Reconfiguring wheels data source to %s",
-             config.wheel_data_source == 0 ? "RPM source" : "Ticks source");
+    double newValue;
+    std::string parameterChanged;
 
-    /**
-     * Setting up the wheels data sources:
-     *      0 - RPM wheels data source (default)
-     *      1 - Ticks wheels data source
-     */
+    switch (level)
+    {
+        case 0:
+            parameterChanged = "Wheel radius [m]";
+            newValue         = config.R;
+            VelocityComputer::getInstance().setR(newValue);
+            break;
+        case 1:
+            parameterChanged = "Wheel position along x [m]";
+            newValue         = config.L;
+            VelocityComputer::getInstance().setL(newValue);
+            break;
+        case 2:
+            parameterChanged = "Wheel position along y [m]";
+            newValue         = config.W;
+            VelocityComputer::getInstance().setW(newValue);
+            break;
+        case 3:
+            parameterChanged = "Gear ratio from motor to wheel";
+            newValue         = config.T;
+            VelocityComputer::getInstance().setT(newValue);
+            break;
+        case 4:
+            parameterChanged = "Counts per revolution of the motor";
+            newValue         = config.N;
+            VelocityComputer::getInstance().setN(newValue);
+            break;
+    }
+
     VelocityComputer::getInstance().setComputeMethod(
         config.wheel_data_source == 0
             ? VelocityComputer::ComputeMethod::RMP
             : VelocityComputer::ComputeMethod::ENCODER);
+
+    ROS_INFO(
+        "[VelocityComputer] Reconfiguring robot parameter: %s; new value: %lf",
+        parameterChanged.c_str(), newValue);
 }
--- src/project_1/src/wheels_velocity_computer.cpp
+++ src/project_1/src/wheels_velocity_computer.cpp
@@ -1,10 +1,13 @@
+#include <dynamic_reconfigure/server.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <project_1/WheelsRpm.h>
+#include <project_1/robotPhysicalParametersConfig.h>
 #include <ros/console.h>
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 
 #include <Eigen/Core>
+#include <string>
 
 #include "shared/WheelsVelocityComputer.h"
 
@@ -12,10 +15,14 @@ using namespace ros;
 using namespace sensor_msgs;
 using namespace geometry_msgs;
 using namespace Eigen;
+using namespace dynamic_reconfigure;
+using namespace project_1;
 
 Publisher pub;
 
 void robotVelocityCallback(const TwistStamped::ConstPtr &msg);
+void robotParametersChangeCallback(robotPhysicalParametersConfig &config,
+                                   uint32_t level);
 
 int main(int argc, char **argv)
 {
@@ -23,7 +30,13 @@ int main(int argc, char **argv)
     NodeHandle handle;
 
     Subscriber sub = handle.subscribe("cmd_vel", 1000, robotVelocityCallback);
-    pub            = handle.advertise<project_1::WheelsRpm>("wheels_rpm", 1000);
+    pub            = handle.advertise<WheelsRpm>("wheels_rpm", 1000);
+
+    // Robot paramters recofiguration
+    Server<robotPhysicalParametersConfig> robotParamsServer;
+    Server<robotPhysicalParametersConfig>::CallbackType robotParamsCallback;
+    robotParamsCallback = boost::bind(&robotParametersChangeCallback, _1, _2);
+    robotParamsServer.setCallback(robotParamsCallback);
 
     spin();
 }
@@ -34,11 +47,52 @@ void robotVelocityCallback(const TwistStamped::ConstPtr &msg)
     auto V = WheelsVelocityComputer::getInstance().computeWheelVelocities(msg);
 
     // Publis the message
-    project_1::WheelsRpm wheelsMsg;
+    WheelsRpm wheelsMsg;
     wheelsMsg.header.stamp = msg->header.stamp;
     wheelsMsg.rpm_fl       = V[0];
     wheelsMsg.rpm_fr       = V[1];
     wheelsMsg.rpm_rr       = V[3];
     wheelsMsg.rpm_rl       = V[2];
     pub.publish(wheelsMsg);
+}
+
+void robotParametersChangeCallback(robotPhysicalParametersConfig &config,
+                                   uint32_t level)
+{
+    double newValue;
+    std::string parameterChanged;
+
+    switch (level)
+    {
+        case 0:
+            parameterChanged = "Wheel radius [m]";
+            newValue         = config.R;
+            WheelsVelocityComputer::getInstance().setR(newValue);
+            break;
+        case 1:
+            parameterChanged = "Wheel position along x [m]";
+            newValue         = config.L;
+            WheelsVelocityComputer::getInstance().setL(newValue);
+            break;
+        case 2:
+            parameterChanged = "Wheel position along y [m]";
+            newValue         = config.W;
+            WheelsVelocityComputer::getInstance().setW(newValue);
+            break;
+        case 3:
+            parameterChanged = "Gear ratio from motor to wheel";
+            newValue         = config.T;
+            WheelsVelocityComputer::getInstance().setT(newValue);
+            break;
+        case 4:
+            parameterChanged = "Counts per revolution of the motor";
+            newValue         = config.N;
+            WheelsVelocityComputer::getInstance().setT(newValue);
+            break;
+    }
+
+    ROS_INFO(
+        "[WheelsVelocityComputer] Reconfiguring robot parameter: %s; new "
+        "value: %lf",
+        parameterChanged.c_str(), newValue);
 }
