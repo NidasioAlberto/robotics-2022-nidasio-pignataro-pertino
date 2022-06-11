1e4c404a534dfdcfc1e759c5d8b2457bcd78cb41
Paolo Pertino <paolo.pertino@mail.polimi.it>
2022-06-11 12:34:10 +0200

    [Mapping] first attempt to mapping

--- src/project_2/src/tf_tree_computer.cpp
+++ src/project_2/src/tf_tree_computer.cpp
@@ -29,13 +29,9 @@ int main(int argc, char **argv)
 void odomCallback(const nav_msgs::Odometry &odometry)
 {
     geometry_msgs::TransformStamped broadcastOdom;
-    geometry_msgs::TransformStamped broadcastBaseLink;
-    tf2::Quaternion quat;
 
-    broadcastOdom.header.frame_id     = "map";
-    broadcastOdom.child_frame_id      = "odom";
-    broadcastBaseLink.header.frame_id = "odom";
-    broadcastBaseLink.child_frame_id  = "base_link";
+    broadcastOdom.header.frame_id = "odom";
+    broadcastOdom.child_frame_id  = "base_link";
 
     // Odom translates and rotates
     broadcastOdom.transform.translation.x = odometry.pose.pose.position.x;
@@ -43,16 +39,5 @@ void odomCallback(const nav_msgs::Odometry &odometry)
     broadcastOdom.transform.translation.z = odometry.pose.pose.position.z;
     broadcastOdom.transform.rotation      = odometry.pose.pose.orientation;
 
-    // Base link doesn't transform in respect to odom
-    quat.setEuler(0, 0, 0);
-    broadcastBaseLink.transform.rotation.x    = quat.getX();
-    broadcastBaseLink.transform.rotation.y    = quat.getY();
-    broadcastBaseLink.transform.rotation.z    = quat.getZ();
-    broadcastBaseLink.transform.rotation.w    = quat.getW();
-    broadcastBaseLink.transform.translation.x = 0;
-    broadcastBaseLink.transform.translation.y = 0;
-    broadcastBaseLink.transform.translation.z = 0;
-
     tf_pub->sendTransform(broadcastOdom);
-    tf_pub->sendTransform(broadcastBaseLink);
 }
