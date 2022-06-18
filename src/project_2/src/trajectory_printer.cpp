8d0345d46898c62e0cedd85264c6f34d96c6c870
Paolo Pertino <paolo.pertino@mail.polimi.it>
2022-06-18 09:45:18 +0200

    [TrajectoryOnMap] added colors and continuous trajectory

--- src/project_2/src/trajectory_printer.cpp
+++ src/project_2/src/trajectory_printer.cpp
@@ -66,23 +66,35 @@ void mapUpdateCallback(const nav_msgs::OccupancyGrid &updatedMap) {
 bool saveMapWithTrajectory(project_2::SaveRobotTrajectory::Request &req, project_2::SaveRobotTrajectory::Response &res) {
     ROS_INFO("Received request to save the map with the trajectory.");
     int pixelX, pixelY;
+    int lastPixelXColored = -1;
+    int lastPixelYColored = -1;
 
-    //pixelX = (realX + originX)/res;
-    //pixelY = (realY + originY)/res;
-
-    cv::Mat flippedImg;
     cv::Mat imageFromOccupancyGrid = occupancyGridToCvMat(currentMap);
 
+    // Setting up the color for the trajectory
+    cv::Vec3b trajectoryColor;
+    trajectoryColor[0] = 0;
+    trajectoryColor[1] = 153;
+    trajectoryColor[2] = 0;
+
+
     for(size_t i=0; i < robotTrajectory.poses.size(); i++) {
-        pixelX = (robotTrajectory.poses[i].pose.position.x + 40) / 0.05;
-        pixelY = (robotTrajectory.poses[i].pose.position.y + 40) / 0.05;
-        //ROS_INFO("Pixel X: %d, Pixel Y: %d", pixelX, pixelY);
-        imageFromOccupancyGrid.data[1600*pixelY+pixelX] = 0;
+        pixelX = (robotTrajectory.poses[i].pose.position.x + abs(currentMap.info.origin.position.x)) / currentMap.info.resolution;
+        pixelY = (robotTrajectory.poses[i].pose.position.y + abs(currentMap.info.origin.position.y)) / currentMap.info.resolution;
+
+        imageFromOccupancyGrid.at<cv::Vec3b>(cv::Point(pixelX,pixelY)) = trajectoryColor;
+
+        if(lastPixelXColored != -1 && lastPixelYColored != -1) {
+            cv::line(imageFromOccupancyGrid, cv::Point(lastPixelXColored, lastPixelYColored), cv::Point(pixelX, pixelY), trajectoryColor);
+        }
+
+        lastPixelXColored = pixelX;
+        lastPixelYColored = pixelY;
     }
 
-    cv::flip(imageFromOccupancyGrid,flippedImg, 0);
-    //cv::rotate(flippedImg, cv::ROTATE_90_COUNTERCLOCKWISE);
-    cv::imwrite("/home/paolo/Scrivania/lol.png", flippedImg);
+    cv::flip(imageFromOccupancyGrid,imageFromOccupancyGrid, 0);
+    cv::rotate(imageFromOccupancyGrid, imageFromOccupancyGrid, cv::ROTATE_90_COUNTERCLOCKWISE);
+    cv::imwrite("/home/paolo/Scrivania/map_with_trajectory.png", imageFromOccupancyGrid);
 
     return true;
 }
@@ -92,18 +104,29 @@ cv::Mat occupancyGridToCvMat(const nav_msgs::OccupancyGrid map)
   uint8_t *data = (uint8_t*) map.data.data(),
            testpoint = data[0];
   bool mapHasPoints = false;
-
-  cv::Mat im(map.info.height, map.info.width, CV_8UC1);
   
-  ROS_INFO("Map size %zd", map.data.size());
+  // Creating the image and the color to use.
+  cv::Mat im(map.info.height, map.info.width, CV_8UC3);
+  cv::Vec3b colorBlack;
+  cv::Vec3b colorWhite;
+  cv::Vec3b colorGrey;
   
-
+  colorBlack[0] = 0;
+  colorBlack[1] = 0;
+  colorBlack[2] = 0;
+  colorWhite[0] = 254;
+  colorWhite[1] = 254;
+  colorWhite[2] = 254;
+  colorGrey[0] = 205;
+  colorGrey[1] = 205;
+  colorGrey[2] = 205;
+  
   // transform the map in the same way the map_saver component does
   for (size_t i=0; i<map.data.size(); i++)
   {
-    if (data[i] == 0)        im.data[i] = 254;
-    else if (data[i] == 100) im.data[i] = 0;
-    else im.data[i] = 205;
+    if (data[i] == 0)        im.at<cv::Vec3b>(cv::Point(i%currentMap.info.width,i/currentMap.info.height)) = colorWhite;
+    else if (data[i] == 100) im.at<cv::Vec3b>(cv::Point(i%currentMap.info.width,i/currentMap.info.height)) = colorBlack;
+    else im.at<cv::Vec3b>(cv::Point(i%currentMap.info.width,i/currentMap.info.height)) = colorGrey;
 
     // just check if there is actually something in the map
     if (i!=0) mapHasPoints = mapHasPoints || (data[i] != testpoint);
